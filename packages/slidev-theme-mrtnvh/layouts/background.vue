cdbf9979b2d0a7692e88a5e7de37371ee0f4afc9
Maarten Van Hoof <maarten.vanhoof@iodigital.com>
2022-10-26 22:23:28 +0200

    feat(theme): v4
    Add code and background layout

---
+++ packages/slidev-theme-mrtnvh/layouts/background.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { handleBackground } from "../layoutHelper";
+
+const props = defineProps({
+    background: {
+        default: "",
+    },
+});
+
+const style = computed(() => handleBackground(props.background));
+</script>
+
+<template>
+    <div class="slidev-layout background" :style="style">
+        <slot />
+    </div>
+</template>
+
+<style>
+.slidev-layout.background {
+    @apply flex h-full;
+}
+</style>
---
+++ packages/slidev-theme-mrtnvh/layouts/code.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="slidev-layout code">
+        <slot />
+    </div>
+</template>
+
+<style>
+.slidev-layout.code {
+    @apply flex w-full h-full py-6 px-12;
+
+    background-color: hsl(220, 13%, 18%);
+}
+
+.slidev-layout.code > * {
+    @apply flex-1;
+}
+
+</style>
